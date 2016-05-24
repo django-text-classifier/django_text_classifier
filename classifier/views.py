7e0453b83b09e69ed38a8e9ce1fe49d73d77ce4c
CameronB <cboudreau6238@gmail.com>
2016-05-24 14:05:31 -0400

    Views wired for home and classifier (and localhost/)

--- classifier/views.py
+++ classifier/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from rest_framework import viewsets
 from .models import TrainingSet
 from .serializers import TrainingSetSerializer
 
 
+""" User views """
+
+
+class Classifier(TemplateView):
+    template_name = "classifier/classifier.html"
+
+
 def index(request):
     context = {}
     return render(request, 'classifier/index.html', context)
+
+
+"""API Endpoint"""
 
 
 class TrainingSetViewSet(viewsets.ModelViewSet):
