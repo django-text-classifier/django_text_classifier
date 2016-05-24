36b5a92a2ea35871e4220e17d9cd99a81fd904c1
gcrowder <gregory.j.crowder@gmail.com>
2016-05-24 16:34:18 -0400

    Classifier may be null.

--- classifier/models.py
+++ classifier/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class TrainingSet(models.Model):
     body = models.TextField()
     target = models.CharField(max_length=255)
-    classifier = models.CharField(max_length=100)
+    classifier = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return 'Classifier {} --> {}: {}'.format(self.classifier,
